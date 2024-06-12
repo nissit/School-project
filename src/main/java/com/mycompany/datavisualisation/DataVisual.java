e1507920be9d799fd1a6ff205995762aad5205fe
sinke237 <enowsinke@gmail.com>
2024-06-12 17:17:43 +0100

    feat: display tabular data

--- src/main/java/com/mycompany/datavisualisation/DataVisual.java
+++ src/main/java/com/mycompany/datavisualisation/DataVisual.java
@@ -2,7 +2,7 @@ package com.mycompany.datavisualisation;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.awt.*;
+// import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
